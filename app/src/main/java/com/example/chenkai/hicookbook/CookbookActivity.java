40c4a2b2694bb3564591912e63d8a2570f624595
jakieenchan <1054636188@qq.com>
2017-04-14 13:51:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/chenkai/hicookbook/CookbookActivity.java
@@ -0,0 +1,13 @@
+package com.example.chenkai.hicookbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CookbookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cookbook);
+    }
+}
